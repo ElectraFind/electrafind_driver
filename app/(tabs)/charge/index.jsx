de77261d0388c41b7860f33a935cdca42034f158
C.D.Sriwarnasinghe <122434896+Damien08SRI@users.noreply.github.com>
2024-08-31 12:50:17 +0530

    updates

--- app/(tabs)/charge/index.jsx
+++ app/(tabs)/charge/index.jsx
@@ -1,12 +1,22 @@
-import { SafeAreaView, Text, View, Image, ScrollView, Button } from 'react-native'
+import { SafeAreaView, Text, View, Image, ScrollView, Button, TouchableOpacity } from 'react-native'
 import { StyleSheet } from 'react-native'
 import React, {useEffect, useState} from 'react'
+import * as Haptics from 'expo-haptics'
 import Header from '../../screens/charge/Header'
+import Recent from '../../screens/charge/Recent'
+import History from '../../screens/charge/History'
 import { images } from '../../../constants'
 import CustomButton from '../../../components/CustomButton'
 import { BarCodeScanner } from 'expo-barcode-scanner'
 
 export default function ChargeScreen(){
+
+  const [activeButton, setActiveButton] = useState("Recent");
+
+  const handleButtonPress = (buttonName) => {
+    setActiveButton(buttonName);
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+  };
 
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
@@ -50,36 +60,44 @@ export default function ChargeScreen(){
 
   return (
 
-    
     <SafeAreaView>
       <ScrollView>
       <View>
         <View style={styles.headerContainer}>
             <Header/>
         </View>
-        <View>
-          <Text style={{color: 'black', fontSize: 20, marginTop: 200, marginLeft:55, justifyContent: 'center', alignItems: 'center'}} className={'font-pbold'}>Scan the QR code to start charging</Text>
-        </View>
-        <View>
-          <Image
-            source={images.electraFindQR}
-            style={{width: 300, height: 300, display:'flex', justifyContent: 'center', alignItems: 'center', marginTop: 60, marginLeft: 60}}
-          />
-        </View>
-        <View>
-          <CustomButton
-            title= 'Start Charging'
-            containerStyles="mt-10 w-2/3 mx-auto">
-              {/* <View style={styles.barcodebox}>
-                <BarCodeScanner
-                  onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-                  style={{ height: 400, width: 400 }} />
-              </View> */}
-          </CustomButton>
 
-          {/* <Text style={styles.maintext}>{text}</Text>
-          {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />} */}
+        <View style={{flexDirection: "row", justifyContent: "center", marginTop: 120, marginHorizontal: 20, backgroundColor: "#E9E9E9", borderRadius: 20, borderWidth: 2,borderColor: "#000000",  }}>
+
+          <TouchableOpacity style={{flex: 1,alignItems: 'center',justifyContent: 'center',paddingVertical: 14, paddingHorizontal: 55, margin: 2, backgroundColor: activeButton === "Recent" ? "#000000" : "transparent", borderRadius: activeButton === "Recent" ? 15 : 0}}
+            onPress={() => handleButtonPress("Recent")} 
+          >
+            <Text style={{color: activeButton === "Recent" ? "#fff" : "#000",}} className={"font-psemibold"}>Recent</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={{flex: 1,alignItems: 'center',justifyContent: 'center',paddingVertical: 14, paddingHorizontal: 55, margin: 2, backgroundColor: activeButton === "History" ? "#000000" : "transparent", borderRadius: activeButton === "History" ? 15 : 0}}
+            onPress={() => handleButtonPress("History")} 
+          >
+            <Text style={{color: activeButton === "History" ? "#fff" : "#000",}} className={"font-psemibold"}>History</Text>
+          </TouchableOpacity>
+
         </View>
+        
+        {
+          activeButton === "Recent" && (
+            <View style={{marginHorizontal: 16, marginVertical: 25}}>
+              <Recent/>
+            </View>
+          )
+        }
+
+        {
+          activeButton === "History" && (
+            <View style={{marginHorizontal: 16, marginVertical: 25}}>
+              <History/>
+            </View>
+          )
+        }
 
       </View>
       </ScrollView>
@@ -96,9 +114,8 @@ const styles = StyleSheet.create({
     padding: 60,
     width: '100%',
     paddingHorizontal: 10,
-    height: '25%',
-    backgroundColor: '#161622'
-    
+    paddingBottom: 10,
+
   },
   maintext: {
     fontSize: 16,
--- app/screens/charge/Header.jsx
+++ app/screens/charge/Header.jsx
@@ -1,14 +1,11 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
+import { View, Text, Image, StyleSheet, } from 'react-native'
 import React from 'react'
-import { images } from '../../../constants'
 
 export default function Header() {
+
   return (
     <View  style={styles.container}>
-      <Image 
-          source={images.logoverticalshort}
-          style={{width:250,height:70,objectFit:'contain',marginTop:0}}
-        />
+      <Text style={{color: 'black', fontSize: 25, fontWeight: 'bold'}}>CHARGE YOUR EV</Text>
     </View>
   )
 }
